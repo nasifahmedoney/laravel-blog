163cece75c87ce6ff74d8eda7e894e735343864c
nasifahmedoney <nasif.ahmed@live.com>
2021-10-07 19:32:40 +0600

    using layout

---
+++ blog/resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+
+        {{-- <header>
+            @yield('banner')
+        </header> --}}
+
+        @yield('content')
+    </body>
+</html>
+
--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,15 +1,17 @@
-<html>
-    <head>
-        <title>Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-        @foreach ($posts_var_from_route as $post)
-        <article>
-            <a href="/post/{{$post->slug}}"><h1>{{$post->title}}</h1></a>
-            <p>{!! $post->body !!}</p> <!-- !! for fetching html content --> 
-        </article>
-        @endforeach
-    </body>
-</html>
+@extends('layout')
+
+{{-- @section('banner')
+    <h1 style="text-align: center">my blog</h1>
+@endsection --}}
+
+@section('content')
+    @foreach ($posts_var_from_route as $post)
+    <article>
+        <a href="/post/{{$post->slug}}"><h1>{{$post->title}}</h1></a>
+        <p>{!! $post->body !!}</p> <!-- !! for fetching html content --> 
+    </article>
+    @endforeach
+@endsection
+    
+  
 
