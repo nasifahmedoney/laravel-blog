ebeb1cea916e97239f1bf4ec9ea4a26e405137b6
nasifahmedoney <nasif.ahmed@live.com>
2021-10-29 20:55:06 +0600

    excerpt instead body

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -8,7 +8,7 @@
     @foreach ($posts_var_from_route as $post)
     <article>
         <a href="/post/{{$post->id}}"><h1>{{$post->title}}</h1></a>
-        <p>{!! $post->body !!}</p> <!-- !! for fetching html content --> 
+        <p>{!! $post->excerpt !!}</p> <!-- !! for fetching html content --> 
     </article>
     @endforeach
 @endsection
