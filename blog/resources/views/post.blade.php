928968721cef7cba731ffa44df35cd058375ffad
nasifahmedoney <nasif.ahmed@live.com>
2021-11-06 19:55:56 +0600

    user name of the post

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <article>
         <h1>{{$post->title}}</h1>
-        <p><a href="/categories/{{$post->category->slug}}">{{$post->category->name}} </a></p>
+        <p>
+            By <a href="#">{{$post->user->name}}</a> in    
+            <a href="/categories/{{$post->category->slug}}">{{$post->category->name}} </a>
+        </p>
         <p>{!! $post->body !!}</p>    
     </article>
 
