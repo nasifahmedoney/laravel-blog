a2c5f14ff42c4375641480d86b56c25b9aba5c98
nasifahmedoney <nasif.ahmed@live.com>
2021-11-06 18:11:18 +0600

    accessing category from Post model (does not work-url issue)

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <article>
         <h1>{{$post->title}}</h1>
+        <p><a href="categories/{{$post->category->slug}}">{{$post->category->name}} </a></p>
         <p>{!! $post->body !!}</p>    
     </article>
 
