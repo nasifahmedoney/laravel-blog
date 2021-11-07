c284b7fd461f2ed1bc3446fd80c2e08767af3f39
nasifahmedoney <nasif.ahmed@live.com>
2021-11-07 18:58:16 +0600

    with property

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -11,6 +11,8 @@ class Post extends Model
 
     protected $guarded = [];
 
+    protected $with = ['category','author'];
+
     //protected $fillable = ['title','excerpt','body','slug','category_id']; #mass assignment only these fields
     //protected $guarded = ['id']; #mass assignment except these fields
 
