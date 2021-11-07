b68f7ed9f8f6ecdb715c4bfef2424ee2beaea806
nasifahmedoney <nasif.ahmed@live.com>
2021-11-07 17:54:48 +0600

    user to author

--- blog/resources/views/post.blade.php
+++ blog/resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h1>{{$post->title}}</h1>
         <p>
-            By <a href="#">{{$post->user->name}}</a> in    
+            By <a href="#">{{$post->author->name}}</a> in    
             <a href="/categories/{{$post->category->slug}}">{{$post->category->name}} </a>
         </p>
         <p>{!! $post->body !!}</p>    
