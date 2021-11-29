faee2f780db69b36133a698f49f63031d026d960
nasifahmedoney <nasif.ahmed@live.com>
2021-11-29 17:59:31 +0600

    login logout with @auth and @guest

--- blog/app/Http/Controllers/RegisterController.php
+++ blog/app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -37,8 +38,12 @@ class RegisterController extends Controller
 
         //option 3 using mutators in User model 
 
-        User::create($attributes);
-
+        $user = User::create($attributes);
+        
+        //login the user created
+        Auth::login($user);
+        
+        
         return redirect('/')->with('success','your account has been created.');
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,8 +33,11 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('post/{post:slug}', [PostController::class, 'show'] );
 
-Route::get('register', [RegisterController::class, 'create'] );
-Route::post('register', [RegisterController::class, 'store'] );
+Route::get('register', [RegisterController::class, 'create'] )->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'] )->middleware('guest');
+//middleware->app/Http/kernel.php
+
+Route::post('logout',[SessionsController::class, 'destroy']);
 
 
 // Route::get('categories/{category:slug}',function(Category $category)
